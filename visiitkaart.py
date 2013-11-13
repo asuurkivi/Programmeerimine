d35f4da95878d5a11eb413a2a9c00d9df4ac685b
Algis Suurkivi <asuurkivi@gmail.com>
2013-11-13 11:01:27 +0200

    Create visiitkaart.py

---
+++ visiitkaart.py
@@ -0,0 +1,22 @@
+pealis= "|--------------------------------------|"
+tyhi= "|                                      |"
+nimi = "|   algis suurkivi                     |"
+email= "|   asuurkivi@gmail.com                |"
+mob= "|   53992793                           |"
+fb= "|   facebook.com/asuurkivi             |"
+twitter="|   @asuurkivi                         |"
+jalus= "|--------------------------------------|"
+
+print pealis
+print tyhi
+print tyhi
+print tyhi
+print nimi.title()
+print email
+print mob
+print fb
+print twitter
+print tyhi
+print tyhi
+print tyhi
+print jalus
