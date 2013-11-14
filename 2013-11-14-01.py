c9ddcadb56fc701915fc4bccd360f87b9d1947e3
Algis Suurkivi <asuurkivi@gmail.com>
2013-11-14 13:50:29 +0200

    Create 2013-11-14-01.py

---
+++ 2013-11-14-01.py
@@ -0,0 +1,14 @@
+x1 = 10
+y1 = 15
+x2 = 25
+y2 = 30
+
+x = 32
+y = 32
+
+if x1 <= x and x <= x2\
+and y1 <=y and y <= y2:
+	print "punkt ({},{}) asub piirkonnas.".format(x,y)
+else:
+	
+	print "ei ole piirkonnas."
