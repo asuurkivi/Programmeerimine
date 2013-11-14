b72c07832808edb0342ac785bde97736bca047a9
Algis Suurkivi <asuurkivi@gmail.com>
2013-11-14 15:25:12 +0200

    Update 2013-11-14-05.py

--- 2013-11-14-05.py
+++ 2013-11-14-05.py
@@ -3,7 +3,8 @@ pw = getpass.getpass("Testi oma salas6na tugevust. Sisesta salas6na: ")
 if not pw.isalpha() \
 	and not pw.isupper() \
 	and not pw.islower() \
-	and len(pw)>=8 :
+	and not pw.isalnum() \
+	and not len(pw)<8 :
 	print "salas6na on tugev"
 else:
 	print "salas6na on n6rk"
